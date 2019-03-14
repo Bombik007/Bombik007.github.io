30c0b3e23356ef0ffdd30651794ff4b718ca97c6
Bombik007 <bodiababiak@gmail.com>
2019-03-14 12:08:38 +0200

    Init

---
+++ precache-manifest.d7a6b87239563002f7127deea67ee8b7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "ae92564cfe90425687a7c1226919241d",
+    "url": "/static/media/logo.ae92564c.svg"
+  },
+  {
+    "revision": "d5439af27c9379789eb57af14f6d73c0",
+    "url": "/static/media/home.d5439af2.svg"
+  },
+  {
+    "revision": "efe3a71a0701efc1c1dedf97b68d10de",
+    "url": "/static/media/arrow_back.efe3a71a.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "03b327ecb1d3697f51da",
+    "url": "/static/js/main.03b327ec.chunk.js"
+  },
+  {
+    "revision": "6326ba016f9e2d96bca3",
+    "url": "/static/js/1.6326ba01.chunk.js"
+  },
+  {
+    "revision": "03b327ecb1d3697f51da",
+    "url": "/static/css/main.dab72b23.chunk.css"
+  },
+  {
+    "revision": "a8b85b5c8fc38e8b46766a1efcc7513a",
+    "url": "/index.html"
+  }
+];
